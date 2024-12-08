5042b158b6b4e9ae6946dabc8f796f83ae69c9f2
DongHoKim <cha11enge@hanmail.net>
2024-12-08 22:09:10 +0900

    기본 설정

---
+++ src/main/kotlin/com/study/project/StudyProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.study.project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyProjectApplication>(*args)
+}
